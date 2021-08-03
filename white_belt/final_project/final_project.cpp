c92c9c3bbfb6b7ab97a46abc7f72bb682c998c6c
Kirill Tamogashev <tamogachev@me.com>
2021-08-04 00:17:54 +0300

    almost finished the project

--- white_belt/final_project/final_project.cpp
+++ white_belt/final_project/final_project.cpp
@@ -47,6 +47,14 @@ istream& operator>>(istream& stream, Date& date) {
   if (stream && div1 == '-' && div2 == '-') {
     date = Date(year, month, day);
   }
+  return stream;
+}
+
+ostream& operator<<(ostream& stream, const Date& date) {
+  stream << date.GetYear() << '-';
+  stream << date.GetMonth() << ' ';
+  stream << date.GetDay() << endl;
+  return stream;
 }
 
 
@@ -62,37 +70,52 @@ bool operator<(const Date& lhs, const Date& rhs) {
   }
 }
 
-// TODO: finish class Database
 class Database {
 public:
+
   void AddEvent(const Date& date, const string& event) {
     databaza[date].insert(event);
   }
+
   bool DeleteEvent(const Date& date, const string& event) {
+    if (databaza[date].count(event) == 1) {
     databaza[date].erase(event);
-  }
-  int  DeleteDate(const Date& date){
-    databaza.erase(date);
+    return true;
+    }
+    return false;
   }
 
-  void Find(const Date& date) { // !!!there was constant mwthod here
-    for (auto ev :databaza[date]) {
+  int  DeleteDate(const Date& date){
+    int len = databaza[date].size();
+    databaza.erase(date);
+    return len;
+  }
+
+  void Find(const Date& date) { 
+    for (const auto ev : databaza[date]) {
       cout << ev << endl;
     }
   }
+
   // TODO print
-  void Print() const;
+  void Print() const {
+    for (const auto elem : databaza) {
+      for (const auto ev : elem.second) {
+        cout << elem.first << " " << ev << endl;
+      }
+    }
+  }
 
 private:
   map<Date, set<string>> databaza;
 };
 
 
-// TODO: add preprocessing of all commands, necessary conditions and exceptions
+// TODO: add exceptions
 int main() {
   Database db;
-    
   string command;
+
   while (getline(cin, command)) {
     // Считайте команды с потока ввода и обработайте каждую
     cin >> command;
@@ -100,26 +123,38 @@ int main() {
       Date date;
       string event;
       cin >> date >> event;
-      // TODO: add date and event ot dataset
+      db.AddEvent(date, event);
 
     } else if (command == "Del") {
       Date date;
       string event = "no_event";
       cin >> date >> event;
+
       if (event == "no_event") {
-        // ! delete all events for the day
+
+        int res = db.DeleteDate(date);
+        cout  << "Deleted " << res << " events" << endl;
       } else {
-        // ! delete specific event
+        bool res = db.DeleteEvent(date, event);
+        if (res) {
+          cout << "Deleted successfully" << endl;
+        } else {
+          cout << "Event not found" << endl;
+        }
       }
+
     } else if (command == "Find") {
       Date date;
       cin >> date;
+      db.Find(date);
 
     } else if (command == "Print") {
+      db.Print();
 
     } else {
       cout << "Unknown command: " <<  command << endl;
     }
   }
+
   return 0;
 }
